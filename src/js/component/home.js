09a5acded6f875778e951fca6f424390bcf52705
Chrischris3018 <chrischrischris3018@yahoo.com>
2020-02-13 01:27:22 +0000

    firts

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,4 +1,8 @@
 import React from "react";
+import { Navbar } from "./navbar";
+import { Card } from "./card";
+import { Jumbotron } from "./jumbotron";
+import { Footer } from "./footer";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -6,19 +10,26 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<Navbar />
+			<Jumbotron />
+			<div className="d-flex justify-content-center">
+				<Card
+					imgUrl="https://randomuser.me/api/portraits/men/8.jpg"
+					title="4Geeks Academy"
+				/>
+				<Card
+					imgUrl="https://randomuser.me/api/portraits/men/9.jpg"
+					title="Google"
+				/>
+				<Card
+					imgUrl="https://randomuser.me/api/portraits/men/7.jpg"
+					title="Google"
+				/>
+				{/* <Card />
+                <Card /> */}
+			</div>
+			<Footer />
+		</>
 	);
 }
